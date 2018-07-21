fc65c3574c9afccba7dbe6e7d983ea355fd9295b
stivenAigle <aiglestiven@gmail.com>
2018-07-21 23:54:20 +0200

    creation de la camera, et du vecteur de caméra qui pourra contenir les camera créer par le developpeur

---
+++ inc/2dEngine/camera.hpp
@@ -0,0 +1,48 @@
+//
+// Created by joe on 21/07/18.
+//
+#ifndef INC_2DENGINE_CAMERA_HPP
+#define INC_2DENGINE_CAMERA_HPP
+
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
+#include <glm/mat4x4.hpp>
+#include <vector>
+class Camera{
+
+private:
+    glm::vec3 pos;
+    glm::vec3 eye;
+    glm::vec3 up;
+
+    float near, far;
+    float bottom, top;
+    float left, right;
+
+    glm::mat4 perspective;
+    glm::mat4 view;
+
+    static Camera * current;
+    static std::vector<Camera *> cameras;
+public:
+    Camera(const glm::vec3 &pos, const glm::vec3 &eye, const glm::vec3 &up, float bottom,
+           float top, float left, float right, float near = 0, float far = 0);
+
+    const glm::vec3 &getPos() const;
+
+    const glm::vec3 & getEye() const;
+
+    const glm::mat4 &getView() const;
+
+    const glm::vec3 &getUp() const;
+
+    const glm::mat4 &getPerspective() const;
+
+    glm::mat4 getCamView() const;
+
+    static void addCamera(Camera *newCam);
+
+    static void changeCamera(int id);
+};
+
+#endif //INC_2DENGINE_CAMERA_HPP
---
+++ src/camera.cpp
@@ -0,0 +1,64 @@
+//
+// Created by joe on 21/07/18.
+//
+
+#include <glm/detail/type_mat4x4.hpp>
+#include <2dEngine/camera.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+Camera * Camera::current = nullptr;
+
+std::vector<Camera *> cameras;
+const glm::mat4 &Camera::getView() const {
+    return view;
+}
+
+const glm::mat4 &Camera::getPerspective() const {
+    return perspective;
+}
+
+const glm::vec3 &Camera::getPos() const {
+    return pos;
+}
+
+const glm::vec3 & Camera::getEye() const {
+    return eye;
+}
+
+const glm::vec3 &Camera::getUp() const {
+    return up;
+}
+
+Camera::Camera(const glm::vec3 &pos, const glm::vec3 &eye, const glm::vec3 &up, float bottom,
+               float top, float left, float right, float near, float far): pos(pos),
+                                                                           eye(eye),
+                                                                           up(up),
+                                                                           near(near),
+                                                                           far(far),
+                                                                           bottom(bottom),
+                                                                            top(top),
+                                                                           left(left),
+                                                                           right(right) {
+    view = glm::lookAt(pos, eye, up);
+    if(!far){
+
+        perspective = glm::ortho(left, right, bottom, top);
+
+    }else{
+
+        perspective = glm::ortho(left, right, bottom, top, near, far);
+    }
+}
+
+glm::mat4 Camera::getCamView() const {
+    return perspective*view;
+}
+
+void Camera::addCamera(Camera *newCam) {
+    Camera::cameras.push_back(newCam);
+
+}
+
+void Camera::changeCamera(int id) {
+    Camera::current = cameras[id];
+}
